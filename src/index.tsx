0b2fd95197f6d201e2e26f4d176f11ae00ac3792
oupo <oupo.nejiki@gmail.com>
2017-11-13 22:40:15 +0900

    LongLongLCGを実装

--- src/index.tsx
+++ src/index.tsx
@@ -72,6 +72,50 @@ class LCG {
   }
 }
 
+class LongLong {
+  high: number;
+  low: number;
+
+  constructor(high: number, low: number) {
+    this.high = high;
+    this.low = low;
+  }
+
+  add(other: LongLong) {
+    let low = this.low + other.low;
+    let carry = this.low > 0x100000000 ? 1 : 0;
+    this.low = low >>> 0;
+    this.high = (this.high + other.high + carry) >>> 0;
+    return this;
+  }
+
+  mul(other: LongLong) {
+    let a0 = this.low & 0xffff;
+    let a1 = this.low >>> 16;
+    let b0 = other.low & 0xffff;
+    let b1 = other.low >>> 16;
+
+    this.high = (((a1 * b0 + a0 * b1 + (a0 * b0 >>> 16)) >>> 16) + a1 * b1 + Math.imul(this.low, other.high) + Math.imul(other.low, this.high)) >>> 0;
+    this.low = Math.imul(this.low, other.low);
+    return this;
+  }
+}
+
+const A = new LongLong(0x5d588b65, 0x6c078965);
+const B = new LongLong(0, 0x269ec3);
+
+class LongLongLCG {
+  seed: LongLong;
+  
+  constructor(high: number, low: number) {
+    this.seed = new LongLong(high, low);
+  }
+  rand(): number {
+    this.seed.mul(A).add(B);
+    return this.seed.high;
+  }
+}
+
 function seed_hit(seed: number, freqs: number[]) {
   let lcg = new LCG(seed);
   for (let f of freqs) {
